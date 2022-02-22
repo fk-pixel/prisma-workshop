311b57d1d75aaa4c32e42aac57a0da1332d106d4
Fatih Kurt <fthbtlkrt@gmail.com>
2022-02-22 02:05:50 +0100

    reslved error handling with class-valdator

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 /* eslint-disable prettier/prettier */
-import { NestFactory, Reflector } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory, Reflector } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { PrismaClientExceptionFilter } from './prisma-client-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,7 +14,12 @@ async function bootstrap() {
   // apply transform to all responses
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
-
+  // 👇 apply PrismaClientExceptionFilter to entire application, requires HttpAdapterHost because it extends BaseExceptionFilter
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  
+  app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
+  
+  // Swagger config
   const config = new DocumentBuilder()
   .setTitle('NestJS Prisma Workshop')
   .setDescription('API description')
